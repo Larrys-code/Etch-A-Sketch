ab3638c04c86d4b33bf85cb2c57a6e7236053dd9
Larry <38928644+Larrys-code@users.noreply.github.com>
2023-04-07 09:55:29 +0100

    Add color change logic

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const gameContainer = document.getElementById("etchASketchContainer");
-var docFrag = document.createDocumentFragment();
-
-for (i=0; i<16; i++){
-    var div = document.createElement("div");
-    div.className = "gameRow";
-
-    docFrag.appendChild(div);
+function getRGB(string) {
+    if (!string){string = "rgb(255,255,255)"};
+    //if (!string || string === "rgb(0, 0, 0)"){string = "rgb(255,255,255)"};
+    let numbers = string.replaceAll(/([/()rgb])/g, "");
+    let rgbArray = numbers.split(",");
+    return rgbArray;
 };
-gameContainer.appendChild(docFrag);
-const rows = document.querySelectorAll(".gameRow");
-rows.forEach(element => {
-    for (i=0; i<16; i++){
-        var square = document.createElement("div");
-        square.className = "gameBlock";
-        element.appendChild(square);
+
+function randomizeColor(block) {
+    const rgbArray = getRGB(block.style.backgroundColor);
+    let r = Math.floor(Math.random() * (25 - 0 + 1) + 0);
+    let g = Math.floor(Math.random() * (25 - 0 + 1) + 0);
+    let b = Math.floor(Math.random() * (25 - 0 + 1) + 0);
+    rgbArray[0] = rgbArray[0] - r;
+    rgbArray[1] = rgbArray[1] - g;
+    rgbArray[2] = rgbArray[2] - b;
+    console.log(block.style.backgroundColor);
+    block.style.backgroundColor = `rgb(${rgbArray[0]}, ${rgbArray[1]}, ${rgbArray[2]})`;
+};
+
+function makeGame(howManyBlocks){
+    const gameContainer = document.getElementById("etchASketchContainer");
+    while (gameContainer.firstChild) {
+        gameContainer.removeChild(gameContainer.lastChild);
+      }
+    var docFrag = document.createDocumentFragment();
+
+    for (i=0; i<howManyBlocks; i++){
+        var div = document.createElement("div");
+        div.className = "gameRow";
+
+        docFrag.appendChild(div);
     };
-});
+    gameContainer.appendChild(docFrag);
+    const rows = document.querySelectorAll(".gameRow");
+    rows.forEach(element => {
+        for (i=0; i<howManyBlocks; i++){
+            var square = document.createElement("div");
+            square.className = "gameBlock";
+            element.appendChild(square);
+        };
+    });
+};
+
+makeGame(16);
+const blocks = document.querySelectorAll(".gameBlock");
+blocks.forEach(block => {block.addEventListener("mouseover", function(){randomizeColor(this)})});
+
